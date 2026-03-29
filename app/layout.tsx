93324ce86a547b8554942241e029a73d30f1075f
alexandrecommerot <alexandre.commerot@gmail.com>
2026-03-29 16:41:14 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: "CashPilote",
+  description: "Landing page CashPilote",
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
